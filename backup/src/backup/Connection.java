7bc60e9bef5efb9a15397dd446458e89c97e3fb6
João Furriel <joaopedrofump@gmail.com>
2018-03-21 12:33:56 +0000

    local Port

--- backup/src/backup/Connection.java
+++ backup/src/backup/Connection.java
@@ -10,16 +10,38 @@ public class Connection {
 	private MulticastSocket MDB;
 	private MulticastSocket MDR;
 	
+	private String mcAddress;
+	private int mcPort;
+	
 	public Connection(String mcAddress, int mcPort, String mdbAddress, int mdbPort, String mdrAddress, int mdrPort) {
 		
+		
+		this.mcAddress = mcAddress;
+		this.mcPort = mcPort;
+		
 		try {
+			
+			
+			
 			this.MC = new MulticastSocket(mcPort);
+			System.out.println(this.MC.getLocalPort());
 			this.MDB = new MulticastSocket(mdbPort);
 			this.MDR = new MulticastSocket(mdrPort);
 			
 			this.MC.joinGroup(InetAddress.getByName(mcAddress));
 			this.MDB.joinGroup(InetAddress.getByName(mdbAddress));
 			this.MDR.joinGroup(InetAddress.getByName(mdrAddress));
+			
+			System.out.println(this.MC.getPort());
+			System.out.println(this.MC.getRemoteSocketAddress());
+			System.out.println(this.MC.getLocalSocketAddress());
+			
+			System.out.println(InetAddress.getByName(mcAddress));
+			
+			System.out.println(mcAddress);
+			System.out.println(mcPort);
+
+
 			
 		
 		} catch (IOException e) {
@@ -29,7 +51,25 @@ public class Connection {
 	}
 
 	public MulticastSocket getMC() {
-		return MC;
+		return this.MC;
+	}
+	
+	
+
+	public String getMcAddress() {
+		return mcAddress;
+	}
+
+	public void setMcAddress(String mcAddress) {
+		this.mcAddress = mcAddress;
+	}
+
+	public int getMcPort() {
+		return mcPort;
+	}
+
+	public void setMcPort(int mcPort) {
+		this.mcPort = mcPort;
 	}
 
 	public MulticastSocket getMDB() {
--- backup/src/backup/Peer.java
+++ backup/src/backup/Peer.java
@@ -11,23 +11,23 @@ public class Peer {
 	public static double BACKUP_PROTOCOL_VERSION = 1.0;
 	
 	public int id;
-	private Connection connection = new Connection("224.0.0.1", 2300, "224.0.0.2", 2301, "224.0.0.3", 2302);
-	private Dispatcher dispatcher = new Dispatcher(connection.getMC());
+	private Connection connection;
+	private Dispatcher dispatcher;
 	
 	ExecutorService threadPool = Executors.newFixedThreadPool(5);
 	
 	public static void main(String[] args) {
 		
+		System.setProperty("java.net.preferIPv4Stack" , "true");
+		
 		Peer peer = new Peer(Integer.parseInt(args[0]));
 		
 		if(peer.id > 1) {
 			
-			String putchunk = "PUT CHUNK";
-			byte[] buffer = putchunk.getBytes();
-			
-			
+			peer.putChunk();
 			
 		}
+				
 		
 	}
 
@@ -35,10 +35,41 @@ public class Peer {
 		
 		this.id = id;
 		
+		connection = new Connection("224.0.0.1", 2300, "224.0.0.2", 2301, "224.0.0.3", 2302);
+		dispatcher = new Dispatcher(connection.getMC());
+		
 		Thread mcDispatcherThread = new Thread(this.dispatcher);
 		mcDispatcherThread.start();
 		
 		
+	}
+	
+	public void putChunk() {
+		
+		String message = "Test put chunk";
+		byte[] buffer = message.getBytes();
+		
+		System.out.println(message);
+		
+		try {
+			
+			System.out.println(this.connection.getMC().getLocalPort());
+			
+			DatagramPacket packetToSend = new DatagramPacket(
+												buffer, buffer.length,
+												this.connection.getMC().getLocalAddress(), 
+												this.connection.getMC().getLocalPort());
+			
+			System.out.println("Before Packet sent");
+			
+			this.connection.getMC().send(packetToSend);
+			
+			System.out.println("Packet sent");
+			
+			
+		}
+		
+		catch(Exception e) { e.printStackTrace(); }
 	}
 	
 	
@@ -67,12 +98,16 @@ public class Peer {
 		@Override
 		public void run() {
 			
+			System.out.println("Dispatcher thread created");
+			
 			while(true) {
 				
 				try {
 					
 					DatagramPacket receivingPacket = new DatagramPacket(buffer, buffer.length);
 					this.multicastSocket.receive(receivingPacket);
+					
+					System.out.println("Packet Receieved");
 					
 					Runnable handler = new HandlerPutChunk();
 					
