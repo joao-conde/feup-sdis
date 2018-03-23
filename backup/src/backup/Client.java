b335db92cf7d882895d864d6a4097383e1583843
joao-conde <joaoconde_13@hotmail.com>
2018-03-23 17:33:19 +0000

    RMI begin

--- backup/src/backup/Client.java
+++ backup/src/backup/Client.java
@@ -1,10 +1,23 @@
 package backup;
 
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class Client {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    private Client() {}
 
-	}
+    public static void main(String[] args) {
 
+        String host = (args.length < 1) ? null : args[0];
+        try {
+            Registry registry = LocateRegistry.getRegistry(host);
+            Protocol stub = (Protocol) registry.lookup("Hello");
+            String response = stub.sayHello();
+            System.out.println("response: " + response);
+        } catch (Exception e) {
+            System.err.println("Client exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
 }
--- backup/src/backup/Message.java
+++ backup/src/backup/Message.java
@@ -4,62 +4,62 @@ import java.io.ByteArrayInputStream;
 import java.util.Scanner;
 
 public class Message {
-	
+
 	private final static byte CR = 0xD;
 	private final static byte LF = 0xA;
 	private final static String CRLF = new String(new byte[] {CR,LF});
-			
+
 	public static enum MessageType {
-		
+
 		PUTCHUNK("PUTCHUNK"),
 		STORED("STORED");
-		
+
 		public String text;
-		
+
 		private MessageType(String text) {
 			this.text = text;
 		}
-		
+
 		public MessageType getType(String text) {
-			
+
 			if(text.equals(this.text))
 				return this;
 			return null;
-			
+
 		}
-		
+
 	}
-	
+
 	public static class ReplicationDegreeOutOfLimitsException extends Exception {
 
 		private static final long serialVersionUID = 1L;
-		
+
 		public ReplicationDegreeOutOfLimitsException() {
 			super("Replication Degree must be between 1 to 9");
 		}
-		
+
 	}
-	
+
 	public static class ChunkNoException extends Exception {
 
 		private static final long serialVersionUID = 1L;
-		
+
 		public ChunkNoException() {
 			super("Number of chunks in a file should be less than 1 000 000 and more than 0");
 		}
-		
+
 	}
-	
+
 	public static class MessageFields {
-		
+
 		public MessageType messageType;
 		public float protocolVersion;
 		public int senderId;
 		String fileId;
 		int chunkNo;
 		int replicationDegree;
-		
-		
+
+
 		public MessageFields(MessageType messageType, float protocolVersion, int senderId, String fileId, int chunkNo,
 				int replicationDegree) {
 			this.messageType = messageType;
@@ -69,90 +69,90 @@ public class Message {
 			this.chunkNo = chunkNo;
 			this.replicationDegree = replicationDegree;
 		}
-		
+
 	}
-	
-		
+
+
 	private byte[] message;
 	private MessageFields messageFields;
 	private byte[] chunk;
-	
+
 	private Message(MessageFields messageFields, byte[] chunk) {
 		this.messageFields = messageFields;
-		this.chunk = chunk;
+		this.chunk = chunk; 
 	}
-	
+
 	private Message(byte[] message) {
 		this.message = message;
 	}
-	
-	
+
+
 	public static Message buildMessage(MessageFields messageFields, byte[] chunk) throws ReplicationDegreeOutOfLimitsException,ChunkNoException {
-		
+
 		Message result = new Message(messageFields, chunk);
-		
-		if(messageFields.replicationDegree < 1 || messageFields.replicationDegree > 9) 
+
+		if(messageFields.replicationDegree < 1 || messageFields.replicationDegree > 9)
 			throw new ReplicationDegreeOutOfLimitsException();
-		
+
 		if(messageFields.chunkNo < 0 || messageFields.chunkNo > 1000000)
 			throw new ChunkNoException();
-		
+
 		String headerString = messageFields.messageType.text + " " + messageFields.protocolVersion + " " + messageFields.senderId + " " + messageFields.fileId;
-		
+
 		switch (messageFields.messageType) {
-		
+
 			case PUTCHUNK:
-				
-				headerString += (" " + messageFields.chunkNo + " " + messageFields.replicationDegree);  
-				
+
+				headerString += (" " + messageFields.chunkNo + " " + messageFields.replicationDegree);
+
 				byte[] header = headerString.getBytes();
-				
+
 				byte[] finalHeader = new byte[header.length + 2];
-				
+
 				System.arraycopy(header, 0, finalHeader, 0, header.length);
 				finalHeader[finalHeader.length-2] = CR;
 				finalHeader[finalHeader.length-1] = LF;
-				
+
 				result.message = new byte[finalHeader.length + chunk.length];
 				System.arraycopy(finalHeader, 0, result.message, 0, finalHeader.length);
 				System.arraycopy(chunk, 0, result.message, header.length, chunk.length);
-				
+
 				break;
-	
+
 			default:
 				break;
 		}
-		
+
 		return result;
-		
+
 	}
-	
+
 	public static Message processMessage(byte[] message) {
-		
+
 		Message result = new Message(message);
-		
+
 		Scanner scanner = new Scanner(new ByteArrayInputStream(message));
-		
-		
+
+
 		while(!scanner.hasNext(CRLF + " *" + CRLF)) {
-			
+
 			//MessageType messageType = MessageType. scanner.next()
-			
-			
-			
-			
+
+
+
+
 		}
-		
-		
+
+
 		scanner.close();
-		
-		
-		
+
+
+
 		return result;
 	}
-	
-	
-	
+
+
+
 	public byte[] getMessage() {
 		return message;
 	}
@@ -164,9 +164,9 @@ public class Message {
 	public byte[] getChunk() {
 		return chunk;
 	}
-	
-	
-	
-	
-		
+
+
+
+
+
 }
